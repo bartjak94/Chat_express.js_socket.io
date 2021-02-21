39e461eca9e968229cef4cb3a7c6f280b9d0e92e
bartjak94 <enshamrat@gmail.com>
2021-02-21 17:38:36 +0100

    Graphics update

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,50 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const autoName = 'Server BOT';
+
 io.on('connection',socket => {
 
-    //send message only to user calling the 'connection'
-    socket.emit('msg', 'Welcome to my chat');
+    socket.on('joinRoom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
 
-    //send message to everybody else
-    socket.broadcast.emit('msg','Someone has joined the chat');
+        //send message only to user calling the 'connection'
+        socket.emit('msg', formatMessage(autoName,`You joined ${user.room}`));
 
-    //send everyone message about someone elaving the chat
+        //send message to everybody else
+        socket.broadcast.to(user.room).emit('msg', formatMessage(autoName,`${user.username} has joined the chat`));
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+        });
+    })
+
+
+
+    //send everyone message about someone leaving the chat
     socket.on('disconnect', () => {
-        io.emit('msg','Someone has left');
+        const user = userLeave(socket.id);
+        if(user) {
+            io.to(user.room).emit('msg',formatMessage(autoName,`${user.username} has left`));
+        
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+            });
+        };
     });
 
+
+
+
     //Catch coming chat messages
-    socket.on('chatMsg', msg => {
-        console.log(msg);
+    socket.on('chatMsg', message => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('msg', formatMessage(user.username,message));
     })
 
 })
